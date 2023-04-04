edef597dac6ad6cef308bc48f3dda30d0fc45501
Stephan Gerhard <git@unidesign.ch>
2023-04-04 16:09:58 +0200

    Updates

--- src/neuroglancer/ui/default_viewer_setup.ts
+++ src/neuroglancer/ui/default_viewer_setup.ts
@@ -64,10 +64,10 @@ export function setupDefaultViewer() {
       //console.log("viewer state changed");
       if (window && window.parent) {
         //console.log("has window, and parent window", window, window.parent);
-        window.parent.postMessage(
+        /*window.parent.postMessage(
           { type: "state_change", state: JSON.stringify(viewer.state.toJSON()) },
           "*"
-        );
+        );*/
         // console.log("json", viewer.state.toJSON());
         //
       }
